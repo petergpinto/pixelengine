106594e7f99c04cec00300404c5f6dba9e6052a9
Peter Pinto <petergpinto@verizon.net>
2021-10-25 16:02:06 -0400

    Added glm include

--- src/PixelEngine.h
+++ src/PixelEngine.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 namespace PixelEngine {
 
--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,46 @@ int main(void)
 	glfwMakeContextCurrent(window);
 	gladLoadGL(glfwGetProcAddress); //Prevents memory access violation https://stackoverflow.com/questions/67400482/access-violation-executing-location-0x0000000000000000-opengl-with-glad-and-glf
 
+
+	GLuint texture = 0;
+
+	GLsizei width = 2;
+	GLsizei height = 2;
+	GLsizei layerCount = 2;
+	GLsizei mipLevelCount = 1;
+
+	// Read you texels here. In the current example, we have 2*2*2 = 8 texels, with each texel being 4 GLubytes.
+	GLubyte texels[32] =
+	{
+		// Texels for first image.
+		0,   0,   0,   255,
+		255, 0,   0,   255,
+		0,   255, 0,   255,
+		0,   0,   255, 255,
+		// Texels for second image.
+		255, 255, 255, 255,
+		255, 255,   0, 255,
+		0,   255, 255, 255,
+		255, 0,   255, 255,
+	};
+	
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D_ARRAY, texture);
+	// Allocate the storage.
+	glTexStorage3D(GL_TEXTURE_2D_ARRAY, mipLevelCount, GL_RGBA8, width, height, layerCount);
+	// Upload pixel data.
+	// The first 0 refers to the mipmap level (level 0, since there's only 1)
+	// The following 2 zeroes refers to the x and y offsets in case you only want to specify a subrectangle.
+	// The final 0 refers to the layer index offset (we start from index 0 and have 2 levels).
+	// Altogether you can specify a 3D box subset of the overall texture, but only one mip level at a time.
+	glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, 0, width, height, layerCount, GL_RGBA, GL_UNSIGNED_BYTE, texels);
+
+	// Always set reasonable texture parameters
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
