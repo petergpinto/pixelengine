85b21052594faa302511de7bfd498d1642c6bcc7
Peter Pinto <petergpinto@verizon.net>
2021-10-25 15:01:32 -0400

    Moved entry point to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
-﻿// PixelEngine.cpp : Defines the entry point for the application.
+// main.cpp : Defines the entry point for the application.
 //
 
-#include "PixelEngine.h"
+#include "main.h"
 
-/*
 int main(void)
 {
-	/* Initialize the library 
+	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
 	const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
@@ -24,21 +23,21 @@ int main(void)
 		return -1;
 	}
 
-	/* Make the window's context current 
+	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 	gladLoadGL(glfwGetProcAddress); //Prevents memory access violation https://stackoverflow.com/questions/67400482/access-violation-executing-location-0x0000000000000000-opengl-with-glad-and-glf
 
-	/* Loop until the user closes the window 
+	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClearColor(1.0, 1.0, 1.0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		/* Swap front and back buffers 
+		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events 
+		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
@@ -49,4 +48,3 @@ int main(void)
 int WinMain(void) {
 	main();
 }
-*/
