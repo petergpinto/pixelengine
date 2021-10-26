ffe0276100263a58bbfeba5d181c6e59cbc3ce3f
Peter Pinto <petergpinto@verizon.net>
2021-10-26 12:15:43 -0400

    Added test sprite render

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include "main.h"
 #include "PixelEngine.h"
+#include <fstream>
 
 int main(void)
 {
@@ -18,12 +19,31 @@ int main(void)
 	glfwMakeContextCurrent(engine->getWindow());
 	gladLoadGL(glfwGetProcAddress); //Prevents memory access violation https://stackoverflow.com/questions/67400482/access-violation-executing-location-0x0000000000000000-opengl-with-glad-and-glf
 
+	glViewport(0,0,engine->getHeight(), engine->getWidth());
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	SpriteRenderer  *Renderer;
+	ResourceManager::LoadShader("../resources/shaders/sprite.vs", "../resources/shaders/sprite.frag", nullptr, "sprite");
+
+	glm::mat4 projection = glm::ortho<float>(0.0f, static_cast<float>(engine->getWidth()),
+		static_cast<float>(engine->getHeight()), 0.0f, -1.0f, 1.0f);
+	ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+	ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+
+	Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+
+	ResourceManager::LoadTexture("../resources/textures/awesomeface.png", true, "face");
+
 	/* Loop until the user closes the window */
 	while (!PixelEngine::shouldTerminate(engine->getWindow()))
 	{
 		/* Render here */
-		glClearColor(1.0, 1.0, 1.0, 1.0);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		Renderer->DrawSprite(ResourceManager::GetTexture("face"),
+			glm::vec2(static_cast<float>(engine->getWidth()-300), static_cast<float>(engine->getHeight()-400)), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(engine->getWindow());
