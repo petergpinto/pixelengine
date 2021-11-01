31e97c7c501afc76ec25ed845c567523cdcb170f
Peter Pinto <petergpinto@verizon.net>
2021-11-01 14:58:37 -0400

    Added Player header file

---
+++ include/Player.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "GameObject.h"
+#include "texture.h"
+
+class Player : public GameObject {
+public:
+	Player(Texture2D, SpriteRenderer*)
+		void moveLeft(double);
+	void moveRight(double);
+	void moveUp(double);
+	void moveDown(double);
+};
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,22 +1,17 @@
-#pragma once
-#include "GameObject.h"
-#include "texture.h"
+#include "Player.h"
 
-class Player : public GameObject {
-public:
-	Player(Texture2D tex, SpriteRenderer* Renderer) : GameObject (tex, Renderer, Position(), Position(), Size(500.0f, 500.0f)) {
+Player::Player(Texture2D tex, SpriteRenderer* Renderer) : GameObject(tex, Renderer, Position(), Position(), Size(500.0f, 500.0f)) {
 
-	}
-	void moveLeft(double deltaTime) {
-		this->localTransform.pos.x -= 100.0f * deltaTime;
-	}
-	void moveRight(double deltaTime) {
-		this->localTransform.pos.x += 100.0f * deltaTime;
-	}
-	void moveUp(double deltaTime) {
-		this->localTransform.pos.y -= 100.0f * deltaTime;
-	}
-	void moveDown(double deltaTime) {
-		this->localTransform.pos.y += 100.0f * deltaTime;
-	}
-};
+}
+void Player::moveLeft(double deltaTime) {
+	this->localTransform.pos.x -= 100.0f * deltaTime;
+}
+void Player::moveRight(double deltaTime) {
+	this->localTransform.pos.x += 100.0f * deltaTime;
+}
+void Player::moveUp(double deltaTime) {
+	this->localTransform.pos.y -= 100.0f * deltaTime;
+}
+void Player::moveDown(double deltaTime) {
+	this->localTransform.pos.y += 100.0f * deltaTime;
+}
