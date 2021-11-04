0936f0ecd23b582b70583194b173979d1916f7bc
Peter Pinto <petergpinto@verizon.net>
2021-11-04 13:06:56 -0400

    lvalue fix

--- src/Cell.cpp
+++ src/Cell.cpp
@@ -14,5 +14,6 @@ void Cell::tick(double deltaTime) {
 }
 
 void Cell::Render() {
-	renderer->Draw(Texture2D(), glm::vec2(this->anchorPoint->pos.x + pos.x, this->anchorPoint->pos.y + pos.y), glm::vec2(1, 1),0.0f,glm::vec3(0,0,1));
+	Texture2D nulltex = Texture2D();
+	renderer->Draw(nulltex, glm::vec2(this->anchorPoint->pos.x + pos.x, this->anchorPoint->pos.y + pos.y), glm::vec2(1, 1),0.0f,glm::vec3(0,0,1));
 }
